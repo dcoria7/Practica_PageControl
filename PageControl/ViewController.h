895152e59d986cc4f3afe8f466776270e42e04ea
D C <dc@MacBook-Pro-de-D.local>
2014-03-11 19:14:05 -0600

    Initial Commit

---
+++ PageControl/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PageControl
+//
+//  Created by D C on 3/11/14.
+//  Copyright (c) 2014 D C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
